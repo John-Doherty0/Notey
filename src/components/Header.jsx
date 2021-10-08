6d94149c11562240b68f66e49f70bc036b067d23
John-Doherty0 <jjdohertywork@gmail.com>
2021-10-08 17:12:08 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+function Header() {
+  return (
+    <header>
+      <h1> <AssignmentIcon /> Notey</h1>
+    </header>
+  );
+}
+
+export default Header;
